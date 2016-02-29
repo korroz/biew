68ec0059147d4e6d99e615c83461636cd19b87d9
Korroz <korroz.with.git@voidentity.net>
2016-02-29 20:53:18 +0100

    Only serve media files in current directory
    A middleware now prevents serving too many files or deeper directories.

--- backend.js
+++ backend.js
@@ -9,20 +9,33 @@ var app = express();
 // Temporary, the idea is that the server is started the directory with media from cmd line.
 process.chdir(path.join(process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'], 'Pictures'));
 
+var ls = q.denodeify(fs.readdir);
+var stat = q.denodeify(fs.stat);
+var media = ls('./').then(function (files) {
+    return q.all(micromatch(files, '*.{png,jpg,jpeg,mp4,webm,gif}').map(function (file) {
+        return stat(file).then(function (st) { return { name: file, stat: st }; });
+    }));
+})
+.then(function (files) {
+    return files.filter(function (fi) {
+        return fi.stat.isFile();
+    });
+});
+
 app.get('/api/media', function (req, res) {
-    var ls = q.denodeify(fs.readdir);
-    var stat = q.denodeify(fs.stat);
-    ls('./').then(function (files) {
-        return q.all(micromatch(files, '*.{png,jpg,jpeg,mp4,webm,gif}').map(function (file) {
-            return stat(file).then(function (st) { return { name: file, stat: st }; });
-        }));
-    })
-    .then(function (fileInfo) {
-        res.json(fileInfo.filter(function (fi) {
-            return fi.stat.isFile();
-        }));
+    media.then(function (files) {
+        res.json(files);
     }, function (err) {
         res.status(500).json(err);
+    });
+});
+app.use('/api/file', function (req, res, next) {
+    var file = req.url.substr(1);
+    media.then(function (files) {
+        if (files.some(function (f) { return f.name === file; }))
+            next();
+        else
+            res.status(404).end();
     });
 });
 app.use('/api/file', express.static('./'));
