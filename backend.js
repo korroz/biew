2fe0e813d4f639d4843413eddc9b8a7b88977a7b
Korroz <korroz.with.git@voidentity.net>
2016-02-29 20:03:20 +0100

    Filter on file extensions

--- backend.js
+++ backend.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var fs = require('fs');
+var path = require('path');
 var q = require('q');
+var micromatch = require('micromatch');
 
 var app = express();
 
 app.get('/api/media', function (req, res) {
     var ls = q.denodeify(fs.readdir);
     var stat = q.denodeify(fs.stat);
-    ls('./').then(function (files) {
-        return q.all(files.map(function (file) {
-            return stat(file).then(function (st) { return { name: file, stat: st }; });
+    var dirPath = path.join(process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'], 'Pictures');
+    ls(dirPath).then(function (files) {
+        return q.all(micromatch(files, '*.{png,jpg,jpeg,mp4,webm,gif}').map(function (file) {
+            return stat(path.join(dirPath, file)).then(function (st) { return { name: file, stat: st }; });
         }));
     })
     .then(function (fileInfo) {
@@ -17,7 +20,7 @@ app.get('/api/media', function (req, res) {
             return fi.stat.isFile();
         }));
     }, function (err) {
-        res.status(500).end();
+        res.status(500).json(err);
     });
 });
 app.use(express.static('frontend'));
