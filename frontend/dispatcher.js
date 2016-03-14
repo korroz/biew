453c1b157fd7698144c9d7ff2d010a9a653eb285
Korroz <korroz.with.git@voidentity.net>
2016-03-15 00:06:59 +0100

    Build dispatcher using Rx instead of flux

--- frontend/dispatcher.js
+++ frontend/dispatcher.js
@@ -1,3 +1,20 @@
-var Dispatcher = require('flux').Dispatcher;
+var Rx = require('rx');
 
 module.exports = new Dispatcher();
+
+function Dispatcher() {
+    var self = this;
+
+    var _subject = new Rx.Subject();
+
+    self.register = register;
+    self.dispatch = dispatch;
+
+    function register(listener) {
+        return _subject.subscribe(listener);
+    }
+
+    function dispatch(payload) {
+        _subject.onNext(payload);
+    }
+}
