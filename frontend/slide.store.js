6aaf6696216564eab3439e47cf14e0ca6c926b7d
Korroz <korroz.with.git@voidentity.net>
2016-03-15 17:30:29 +0100

    Remove unused code

--- frontend/slide.store.js
+++ frontend/slide.store.js
@@ -7,14 +7,11 @@ module.exports = new SlideStore();
 function SlideStore() {
     var self = this;
 
-    var _changeSubject = new Rx.Subject();
     var _started = false;
     var _timer = Rx.Disposable.empty;
 
     self.start = start;
     self.stop = stop;
-    self.isSliding = isSliding;
-    self.onChange = onChange;
 
     dispatcher.register(_actionHandler);
 
@@ -25,24 +22,16 @@ function SlideStore() {
         }
         _play(true);
     }
+
     function stop() {
         _timer.dispose();
         _play(false);
     }
-    function isSliding() {
-        return _started;
-    }
-
-    function onChange(listener) {
-        return _changeSubject.subscribe(listener);
-    }
 
     function _play(play) {
-        if (play !== _started) {
-            _started = play;
-            _changeSubject.onNext();
-        }
+        _started = play;
     }
+
     function _actionHandler(payload) {
         switch (payload.actionType) {
             case 'slide:start':
