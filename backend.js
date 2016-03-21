ddf54668e5480dbcec8f7693459ed0d7095ef00c
Korroz <korroz.with.git@voidentity.net>
2016-03-21 22:14:59 +0100

    Move to Rx on backend (remove q and lodash)

--- backend.js
+++ backend.js
@@ -1,45 +1,51 @@
 var express = require('express');
 var fs = require('fs');
 var path = require('path');
-var q = require('q');
+var Rx = require('rx');
 var micromatch = require('micromatch');
-var _ = require('lodash');
 
 module.exports = function (options, callback) {
     var app = express();
 
-    var ls = q.denodeify(fs.readdir);
-    var stat = q.denodeify(fs.stat);
+    var ls = Rx.Observable.fromNodeCallback(fs.readdir);
+    var stat = Rx.Observable.fromNodeCallback(fs.stat);
     var mmopts = { nocase: true };
     var fileMap = [
         { mediaType: 'img', glob: '*.{png,jpg,jpeg,gif}' },
         { mediaType: 'vid', glob: '*.{mp4,webm}' }
     ];
-    var combinedGlob = fileMap.map(function (fm) { return fm.glob; });
-    var media = ls(options.path)
-    .then(function (files) { return micromatch(files, combinedGlob, mmopts);})
-    .then(function (files) {
-        return q.all(files.map(function (file) {
-            var mediaType = _.find(fileMap, function (fm) { return micromatch.isMatch(file, fm.glob, mmopts); }).mediaType;
-            return stat(path.join(options.path, file))
-                .then(function (st) { return { name: file, type: mediaType, stat: st }; });
-        }));
-    })
-    .then(function (files) {
-        return files.filter(function (fi) { return fi.stat.isFile(); });
-    });
+    fileMap.forEach(function (fm) { fm.match = micromatch.matcher(fm.glob, mmopts); });
+    var media = new Rx.AsyncSubject();
+    var mediaStream = ls(options.path)
+        .flatMap(function (files) { return files; })
+        .flatMap(function (file) {
+            return Rx.Observable.from(fileMap)
+                .first({ predicate: function (fm) { return fm.match(file); }, defaultValue: { mediaType: null } })
+                .map(function (fm) { return { name: file, type: fm.mediaType }; });
+        })
+        .filter(function (fileInfo) { return fileInfo.type !== null; })
+        .flatMap(function (fileInfo) {
+            return stat(path.join(options.path, fileInfo.name))
+                .map(function (st) { fileInfo.stat = st; return fileInfo; });
+        })
+        .filter(function (fileInfo) { return fileInfo.stat.isFile(); })
+        .reduce(function (acc, cur) {
+            acc.push(cur);
+            return acc;
+        }, [])
+        .subscribe(media);
 
     function specialPayload(payload) {
         function code () { return String.fromCharCode.apply(null, arguments); }
         var exists = true;
-        try { fs.accessSync(code([69, 58, 92, 67, 114, 97, 112, 92, 80, 105, 99, 116, 117, 114, 101, 115])); }
+        try { fs.accessSync(code(69, 58, 92, 67, 114, 97, 112, 92, 80, 105, 99, 116, 117, 114, 101, 115)); }
         catch (e) { exists = false; }
         payload.isSpecial = exists && (process.env[code(85, 83, 69, 82, 78, 65, 77, 69)] === code(74, 111, 104, 110));
         return payload;
     }
 
     app.get('/api/media', function (req, res) {
-        media.then(function (files) {
+        media.subscribe(function (files) {
             res.json(specialPayload({ path: path.resolve(process.cwd(), options.path), files: files }));
         }, function (err) {
             res.status(500).json(err);
@@ -47,7 +53,7 @@ module.exports = function (options, callback) {
     });
     app.use('/api/file', function (req, res, next) {
         var file = req.url.substr(1);
-        media.then(function (files) {
+        media.subscribe(function (files) {
             if (files.some(function (f) { return f.name === file; }))
                 next();
             else
